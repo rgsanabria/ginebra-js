e6c141ba8c5778e7b3f773739026ee4fe584dbff
Rocio Sanabria <rogsanabria@gmail.com>
2023-06-14 21:07:25 -0300

    add Dom JSON

--- main.js
+++ main.js
@@ -1,72 +1,204 @@
-function saludar() {
-    let nombre = prompt("¡Hola! Ingrese su nombre por favor");
-    alert("Bienvenido/a  " + nombre + " a Ginebra");
+const stockBebidas = [
+  {
+    id: 1,
+    nombre: "Aperol",
+    cantidad: 1,
+    desc: "750ml",
+    precio: 1800,
+    img: "img/aperol.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Absolut",
+    cantidad: 1,
+    desc: "700ml",
+    precio: 5500,
+    img: "img/absolut.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Fernet",
+    cantidad: 1,
+    desc: "750ml",
+    precio: 1570,
+    img: "img/fernet.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Smirnoff",
+    cantidad: 1,
+    desc: "700ml",
+    precio: 1650,
+    img: "./img/smirnoff.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Jagermeister",
+    cantidad: 1,
+    desc: "700ml",
+    precio: 7990,
+    img: "img/jager.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Gin Aconcagua",
+    cantidad: 1,
+    desc: "750ml",
+    precio: 2200,
+    img: "img/gin.jpg",
+  },
+  
+];
+let carrito = [];
+
+const contenedor = document.querySelector("#contenedor");
+const carritoContenedor = document.querySelector("#carritoContenedor");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+const activarFuncion = document.querySelector("#activarFuncion");
+const procesarCompra = document.querySelector("#procesarCompra");
+const totalProceso = document.querySelector("#totalProceso");
+const formulario = document.querySelector('#procesar-pago')
+
+
+
+if (vaciarCarrito) {
+  vaciarCarrito.addEventListener("click", () => {
+    carrito.length = [];
+    mostrarCarrito();
+  });
 }
 
-class Bebidas{
-    constructor(marca, precio, tamaño){
-        this.marca =marca;
-        this.precio =precio;
-        this.tamaño =tamaño;
+if (procesarCompra) {
+  procesarCompra.addEventListener("click", () => {
+    if (carrito.length === 0) {
+      Swal.fire({
+        title: "El carrito está vacio",
+        text: "Seleccione un producto",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    } else {
+      location.href = "compra.html";
     }
+  });
 }
 
-let precios = [
-    {marca: "Fernet", precio:"2500"},
-    {marca: "Smirnoff", precio:"3000"},
-    {marca: "Jagger", precio:"4000"},
-    {marca: "CocaCola", precio:"800"},
-    {marca: "Energiaznte", precio:"500"},
-]
+stockBebidas.forEach((prod) => {
+  const { id, nombre, precio, desc, img, cantidad } = prod;
+  if (contenedor) {
+    contenedor.innerHTML += `
+    <div class="card mt-3" style="width: 18rem;">
+    <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${nombre}</h5>
+      <p class="card-text">Precio: ${precio}</p>
+      <p class="card-text">Descripcion: ${desc}</p>
+      <p class="card-text">Cantidad: ${cantidad}</p>
+      <button class="btn btn-dark" onclick="agregarProducto(${id})">Comprar Producto</button>
+    </div>
+  </div>
+    `;
+  }
+});
 
-saludar()
+const agregarProducto = (id) => {
+  const existe = carrito.some(prod => prod.id === id)
 
-function cargarProducto(){
- let marca = prompt("Ingrese la marca de la bebida elegida. Ej: Fernet");
- let precio = parseInt(prompt("Ingrese el precio"));
- let tamaño = prompt("Ingrese el tamaño");
- //creacion de un objeto//
- const nuevaBebida = new Bebidas(marca, precio, tamaño)
- arrayCarrito.push(nuevaBebida)
- alert("Producto añadido en el carrito")
-}
-
-function verCarrito(){
-    arrayCarrito.forEach((producto)=>{
-     alert(`Usted eligio la bebida ${producto.marca} por un total de ${producto.precio} en tamaño ${producto.tamaño}`)
+  if(existe){
+    const prod = carrito.map(prod => {
+      if(prod.id === id){
+        prod.cantidad++
+      }
     })
+  } else {
+    const item = stockBebidas.find((prod) => prod.id === id)
+    carrito.push(item)
+  }
+  mostrarCarrito()
+
+};
+
+const mostrarCarrito = () => {
+  const modalBody = document.querySelector(".modal .modal-body");
+  if (modalBody) {
+    modalBody.innerHTML = "";
+    carrito.forEach((prod) => {
+      const { id, nombre, precio, desc, img, cantidad } = prod;
+      console.log(modalBody);
+      modalBody.innerHTML += `
+      <div class="modal-contenedor">
+        <div>
+        <img class="img-fluid img-carrito" src="${img}"/>
+        </div>
+        <div>
+        <p>Producto: ${nombre}</p>
+      <p>Precio: ${precio}</p>
+      <p>Cantidad :${cantidad}</p>
+      <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+        </div>
+      </div>
+      
+  
+      `;
+    });
+  }
+
+  if (carrito.length === 0) {
+    console.log("Nada");
+    modalBody.innerHTML = `
+    <p class="text-center text-dark parrafo">Carrito vacio</p>
+    `;
+  } else {
+    console.log("Produc");
+  }
+
+  carritoContenedor.textContent = carrito.length;
+
+  if (precioTotal) {
+    precioTotal.innerText = carrito.reduce(
+      (acc, prod) => acc + prod.cantidad * prod.precio,
+      0
+    );
+  }
+
+  guardarStorage();
+};
+
+function guardarStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-function mostrarOferta(){
-    const ofertas = precios.filter((bebida)=> bebida.precio < 3000)
-    for(const oferta of ofertas){
-        alert(`Estas son las ofertas de hoy: bebidas ${oferta.marca} a un total de ${oferta.precio}`)
+function eliminarProducto(id) {
+  const bebidaId = id;
+  carrito = carrito.filter((bebida) => bebida.id !== bebidaId);
+  mostrarCarrito();
+}
+function procesarPedido() {
+  carrito.forEach((prod) => {
+    const listaCompra = document.querySelector("#lista-compra tbody");
+    const { id, nombre, precio, img, cantidad } = prod;
+    if (listaCompra) {
+      const row = document.createElement("tr");
+      row.innerHTML += `
+              <td>
+              <img class="img-fluid img-carrito" src="${img}"/>
+              </td>
+              <td>${nombre}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>${precio * cantidad}</td>
+            `;
+      listaCompra.appendChild(row);
     }
+  });
+  totalProceso.innerText = carrito.reduce(
+    (acc, prod) => acc + prod.cantidad * prod.precio,
+    0
+  );
 }
 
-function finalizarCompra(){
-    const total = arrayCarrito.reduce((acc, el)=> acc +el.precio, 0);
-    alert(`Gracias por su compra, el total es de ${total}`);
-}
 
-let arrayCarrito = []
+ localStorage.clear()
 
-let opcion = prompt("Ingrese una opcion: \n 1: Cargar productos  \n 2: Ver ofertas \n 3: Ver carrito \n 4: Finalizar compra \n 5: Salir ");
-
-while (opcion !== "5") {
-    if (opcion === "1"){
-        cargarProducto(arrayCarrito)
-    }
-    if (opcion === "2") {
-       mostrarOferta()
-    }
-    if (opcion === "3"){
-        verCarrito()
-    }
-    if (opcion === "4"){
-        finalizarCompra()
-    }
-    opcion = prompt ("Ingrese una nueva opcion: \n 1: Seguir comprando  \n 2: Ver ofertas \n 3: Ver carrito \n 4: Finalizar compra \n 5: Salir ")
-}
-alert ("Muchas gracias por su compra");
-
+ 
